eb1118e8ea5b9445afc633207b7bcf2bb6c50251
Reza Alizadeh Majd <r.majd@pantherx.org>
2019-01-01 18:01:02 +0330

    get single package as json script implemented

---
+++ scripts/test.scm
@@ -0,0 +1,13 @@
+(use-modules (pxutils)
+        (guix packages)
+        (json))
+
+(for-each (lambda (pkg) 
+                (if (string=? (package-name pkg) "git")
+                        (display (package-as-json pkg)))
+
+                ; (define jsn (package-as-json pkg))
+                ; (if (string=? (package-name pkg) "git")
+                ;         (display jsn))
+        )
+        (all-packages))
